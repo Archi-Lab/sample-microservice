08a1e6364f59bd3549bcb850eb97f3ec226ed4e1
Jann Deterling <jann.deterling@smail.th-koeln.de>
2018-11-23 14:19:48 +0100

    adds controller methods for pay and cancel

--- src/main/java/de/th/koeln/fae/samplemicroservice/order/controller/OrderController.java
+++ src/main/java/de/th/koeln/fae/samplemicroservice/order/controller/OrderController.java
@@ -6,8 +6,14 @@ import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 import de.th.koeln.fae.samplemicroservice.order.model.Order;
+import de.th.koeln.fae.samplemicroservice.order.model.OrderException;
 import de.th.koeln.fae.samplemicroservice.order.model.OrderStatus;
 import de.th.koeln.fae.samplemicroservice.order.repository.OrderRepository;
 
@@ -20,6 +26,40 @@ public class OrderController {
   public OrderController(
       OrderRepository orderRepository) {
     this.orderRepository = orderRepository;
+  }
+
+  @PostMapping(path = "/orders/{id}/pay")
+  public ResponseEntity<?> postPay(@PathVariable("id") Long id) {
+    final Optional<Order> optionalOrder = this.orderRepository.findById(id);
+    if (optionalOrder.isPresent()) {
+      final Order order = optionalOrder.get();
+      try {
+        order.pay();
+        this.orderRepository.save(order);
+        return ResponseEntity.ok(new Resource<>(order));
+      } catch (OrderException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+      }
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @PostMapping(path = "/orders/{id}/cancel")
+  public ResponseEntity<?> postCancel(@PathVariable("id") Long id) {
+    final Optional<Order> optionalOrder = this.orderRepository.findById(id);
+    if (optionalOrder.isPresent()) {
+      final Order order = optionalOrder.get();
+      try {
+        order.cancel();
+        this.orderRepository.save(order);
+        return ResponseEntity.ok(new Resource<>(order));
+      } catch (OrderException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+      }
+    } else {
+      return ResponseEntity.notFound().build();
+    }
   }
 
   @Bean
